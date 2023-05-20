2b91d15ef6de799feae9e53c5b15ffbcea5199e5
James Campbell <e@mailjames.com>
2023-05-20 15:22:56 +0100

    scan type option

--- ecsClusternmapAutomator.sh
+++ ecsClusternmapAutomator.sh
@@ -1,5 +1,6 @@
 read -p "Which ECS Cluster do you want to scan? " cluster_name
 read -p "Which directory do you want your report to be output to? " output_filepath
+read -p "Which scan type are you looking to perform against the cluster? (Network | Port | Script | Full | UDP | Vulns | Recon | All) " scan_type
 
 if ! command -v aws &> /dev/null
 then
@@ -22,10 +23,10 @@ fi
 echo using aws cli to get private ip addresses of the cluster...
 
 for ip in $(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${cluster_name}*" | jq -r '.Reservations[].Instances[].PrivateIpAddress'); do
-		command="./nmapAutomator.sh -H $ip -t All"
+		command="./nmapAutomator.sh -H $ip -t $scan_type"
 		echo got $ip
 		echo running $command
-		./nmapAutomator.sh -H $ip -t All > $output_filepath/nmap-automator-$ip-date-$(date '+%d-%m-%Y-%H-%M').log
+		./nmapAutomator.sh -H $ip -t $scan_type > $output_filepath/nmap-automator-$ip-date-$(date '+%d-%m-%Y-%H-%M').log
 done
 
 
