0e40ca9369214ed8c8fb9d2332ad87ff7796b4a7
James Campbell <e@mailjames.com>
2023-05-20 15:12:17 +0100

    deps check

--- ecsClusternmapAutomator.sh
+++ ecsClusternmapAutomator.sh
@@ -1,5 +1,23 @@
 read -p "Which ECS Cluster do you want to scan? " cluster_name
 read -p "Which directory do you want your report to be output to? " output_filepath
+
+if ! command -v aws &> /dev/null
+then
+    echo "AWS CLI is not installed, installing"
+    apt-get update && apt-get install awscli -y
+else
+    echo "AWS CLI is installed. Great"
+fi
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq is not installed, installing"
+    apt-get update && apt-get install jq -y
+else
+    echo "jq is installed. Great"
+fi
+
+
 echo using aws cli to get private ip addresses of the cluster...
 
 for ip in $(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${cluster_name}*" | jq -r '.Reservations[].Instances[].PrivateIpAddress'); do
